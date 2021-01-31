d42c73e17294ed7a2b507dad076e94d08bb20a0d
NeverUnknown <strong.darii@mail.ru>
2021-01-31 02:49:01 +0200

    Lab 1

--- Lab_1/src/Main.java
+++ Lab_1/src/Main.java
@@ -4,24 +4,28 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner inD = new Scanner(System.in);
-        System.out.println("Enter Fahrenheit -> ");
-        System.out.println(celsiusToFahrenheit(inD.nextDouble()));
+//        Scanner inD = new Scanner(System.in);
+//        System.out.println("Enter Fahrenheit -> ");
+//        System.out.println(celsiusToFahrenheit(inD.nextDouble()));
+//
+//        Scanner inI = new Scanner(System.in);
+//        System.out.println("Enter number to reverse -> ");
+//        System.out.println(reverse(inI.nextInt()));
+//
+//        System.out.println("Enter number to know if it is a palindrome -> ");
+//        System.out.println(isPalindrome(inI.nextInt()));
+//
+//        System.out.println("Enter number for palindrome -> ");
+//        palindromeNumber(inI.nextInt());
+//
+//        inD.close();
+//        inI.close();
+//
+//        countVowelsAndConsonants("Bruh, look at this dude!");
 
-        Scanner inI = new Scanner(System.in);
-        System.out.println("Enter number to reverse -> ");
-        System.out.println(reverse(inI.nextInt()));
-
-        System.out.println("Enter number to know if it is a palindrome -> ");
-        System.out.println(isPalindrome(inI.nextInt()));
-
-        System.out.println("Enter number for palindrome -> ");
-        palindromeNumber(inI.nextInt());
-
-        inD.close();
-        inI.close();
-
-        countVowelsAndConsonants("Bruh, look at this dude!");
+        System.out.println(getBucketCount(3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(3.4, 2.1, 1.5));
+        System.out.println(getBucketCount(6.26, 2.2));
     }
 
     //Ex1
@@ -83,7 +87,47 @@ public class Main {
 
     //___________________________________________________________________________Extra Ex______________________________________________________________________________
 
+    /**
+     * @param width         double
+     * @param height        double
+     * @param areaPerBucket double
+     * @param extraBuckets  int
+     * @return int
+     */
+    //ex1.1
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets <= 0) {
+            return -1;
+        }
+        return (int) Math.ceil(width * height / areaPerBucket) - extraBuckets;
+    }
 
+    /**
+     * @param width         double
+     * @param height        double
+     * @param areaPerBucket double
+     * @return int
+     */
+    //ex1.2
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+        return (int) Math.ceil(width * height / areaPerBucket);
+    }
+
+    /**
+     * @param area          double
+     * @param areaPerBucket double
+     * @return int
+     */
+    //ex1.3
+    public static int getBucketCount(double area, double areaPerBucket) {
+        if (area <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+        return (int) Math.ceil(area / areaPerBucket);
+    }
 
 
 }
